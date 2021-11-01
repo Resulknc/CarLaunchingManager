0cb9474aa0d8395a9402d97244a250e098d02189
Resul Ekinci <resulekinci10@gmail.com>
2021-11-01 16:16:15 +0300

    EventDto has been created. Buniness Project has been created. Business Layer for Event And Attendee has been implemented.

--- DataAccess/Abstract/IEventDal.cs
+++ DataAccess/Abstract/IEventDal.cs
@@ -1,5 +1,6 @@
 ﻿using Core.DataAccess;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +9,7 @@ namespace DataAccess.Abstract
 {
     public interface IEventDal:IEntityRepository<Event>
     {
-       
+        List<EventDetailDto> GetEventsDtos();
+        List<EventDetailDto> GetEventDto(int eventId);
     }
 }
