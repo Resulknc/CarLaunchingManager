4002dc61e25dd4b7c86b69fba74c33a49d3552c7
mdenizsert <e170503010@stud.tau.edu.tr>
2021-11-03 06:31:25 +0300

    Business layers for Destination and Invitee have been created.

---
+++ Business/Concrete/DestinationManager.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Collections.Generic;
+using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class DestinationManager : IDestinationService
+    {
+        IDestinationDal _destinationDal;
+
+        public DestinationManager(IDestinationDal destinationDal)
+        {
+            _destinationDal = destinationDal;
+        }
+        public IResult Add(Destination destination)
+        {
+            _destinationDal.Add(destination);
+            return new SuccessResult();
+        }
+
+        public IResult Delete(Destination destination)
+        {
+            _destinationDal.Delete(destination);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<Destination>> GetAll()
+        {
+            return new SuccessDataResult<List<Destination>>(_destinationDal.GetAll());
+        }
+
+        public IDataResult<Destination> GetByDestinationId(int id)
+        {
+            return new SuccessDataResult<Destination>(_destinationDal.Get(d => d.DestinationId == id));
+        }
+
+        public IResult Update(Destination destination)
+        {
+            _destinationDal.Update(destination);
+            return new SuccessResult();
+        }
+    }
+}
