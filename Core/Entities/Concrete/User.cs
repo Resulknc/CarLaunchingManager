1a6de8bfbb7ca34bed2d5e5fc0f7e8a50ab7f85f
mdenizsert <e170503010@stud.tau.edu.tr>
2021-11-11 14:03:22 +0300

    User class moved to core layer. For security folders and classes have been created.

--- Core/Entities/Concrete/User.cs
+++ Core/Entities/Concrete/User.cs
@@ -1,17 +1,18 @@
-﻿using Core.Entities;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace Entities.Concrete
+namespace Core.Entities.Concrete
 {
-    public class User:IEntity
+    public class User : IEntity
     {
         public int UserId { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
         public bool IsAdmin { get; set; }
-        public byte[] PasswordSalt  { get; set; }
+        public byte[] PasswordSalt { get; set; }
         public byte[] PasswordHash { get; set; }
         public bool Status { get; set; }
     }
