4e791f908f252a6d0396cf41f9e3cb3770c49de8
mdenizsert <e170503010@stud.tau.edu.tr>
2021-11-11 13:24:33 +0300

    OperationClaim and UserOperationClaim tables have been created. For security several changes have been made fin User table.

--- Entities/Concrete/User.cs
+++ Entities/Concrete/User.cs
@@ -10,7 +10,9 @@ namespace Entities.Concrete
         public int UserId { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
         public bool IsAdmin { get; set; }
+        public byte[] PasswordSalt  { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public bool Status { get; set; }
     }
 }
