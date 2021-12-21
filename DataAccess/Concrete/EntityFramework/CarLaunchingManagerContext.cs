8b891168db305459c871f8a35d721cd00af8cc0a
Resul Ekinci <resulekinci10@gmail.com>
2021-12-21 15:59:54 +0300

    Db checked

--- DataAccess/Concrete/EntityFramework/CarLaunchingManagerContext.cs
+++ DataAccess/Concrete/EntityFramework/CarLaunchingManagerContext.cs
@@ -10,7 +10,7 @@ namespace DataAccess.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=CarLaunchingManager;Trusted_Connection=true");
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=CLManager;Trusted_Connection=true");
 
         }
 
